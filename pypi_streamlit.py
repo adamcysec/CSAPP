de37990a9b79befaba814fc1a4511e347fb5bae5
Adam Ponce <94799223+adamcysec@users.noreply.github.com>
2023-05-28 16:16:48 -0500

    Add files via upload

---
+++ pypi_streamlit.py
@@ -0,0 +1,96 @@
+import duckdb
+import streamlit as st
+
+db = "duck.db"
+destination_table_name = "pypi"
+filename = "05-21-2023_pypy_info_main_db_audit.csv"
+
+def execute_query(query: str, db: str, return_type: str = "df"):
+    with duckdb.connect(db, read_only=True) as con:
+        if return_type == "df":
+            return con.execute(query).df()
+        elif return_type == "arrow":
+            return con.execute(query).arrow()
+        elif return_type == "list":
+            return con.execute(query).fetchall()
+
+
+#@st.experimental_memo  # An optimization wrapper to memoize the result of the function
+@st.cache_data
+def export_df(df):
+    return df.to_csv(index=False).encode("utf-8")
+
+def load_file(db: str = "duck.db", infile_path: str = "05-21-2023_pypy_info_main_db_audit.csv", table_name: str = "pypi"):
+    with duckdb.connect(db) as conn:
+        conn.execute(f"CREATE OR REPLACE TABLE {table_name} as SELECT * FROM read_csv_auto('{infile_path}')")
+    return True
+
+def main():
+    st.title("Pypi Threat Hunting")
+    try:
+        #button = st.button(label="Get Started")
+        #if button:
+        load_file(db=db, infile_path=filename, table_name=destination_table_name)
+
+        data = execute_query(f"select * from {destination_table_name}", db=db, return_type="df")
+        ###########  section 1  ###########  
+        st.write("## Data Sample")
+        st.dataframe(data.head(10), height=300)
+
+        ###########  section 2  ###########
+        st.write("## Visualization")
+        option = st.selectbox("Select a dimension", ["rank", "stars", "dependent_repos_count", "dependents_count", "forks", "total_versions"], key="option")
+        if option:
+            #st.write(f"### Bar Chart: {option} x Quantity")
+            #st.bar_chart(data[option], x=option, y="total_versions")
+
+            #st.write(f"### Bar Chart: {option} x Amount")
+            #st.bar_chart(data[option], x=option, y="amount")
+
+            st.write(f"### Bar Chart: {option} x Count")
+            st.bar_chart(data[option].value_counts())
+
+        ###########  section 3  ###########
+        st.write("## Filters ##")
+        filter_option = st.selectbox("Select a filter", ["rank", "licenses", "normalized_licenses"], key="filter_option")
+        st.write(f"### Filters (by {filter_option})")
+        value_list = [
+            row[0]
+            for row in execute_query(
+                f"select distinct({filter_option}) from {destination_table_name}", db=db, return_type="list"
+            )
+        ]
+        value_list.sort()
+        
+        filter_by = st.selectbox(label="Select a Value", options=value_list, key="rank_filter")
+        
+        if filter_by != "--":
+            if "'" in filter_by:
+                filter_by = filter_by.replace("'", "''")
+
+            result = execute_query(
+                f"select * from {destination_table_name} where {filter_option} = '{filter_by}'",
+                db=db,
+                return_type="df",
+            )
+            st.dataframe(result, height=400)
+
+            #st.write(f"select * from {destination_table_name} where {filter_option} = '{filter_by}'")
+            st.write(f"Total Records: {len(result)}")
+
+
+            # To download the data we have just selected
+            st.write("#### Download Data")
+            st.download_button(
+                label="Press to Download",
+                data=export_df(result),
+                file_name=f"pypi - rank='{filter_by}'.csv",
+                mime="text/csv",
+                key="download-csv",
+            )
+    except duckdb.CatalogException:  # Catch exception when the database file don't exist yet
+        st.text("Please Clik on the above button to start hunting.")
+
+if __name__ == "__main__":
+    
+    main()
